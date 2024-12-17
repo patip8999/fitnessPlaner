1f11c5ba620723ba82f8784a0099f139ae124815
your_name <patip8999@gmail.com>
2024-12-17 19:29:48 +0100

    firebase works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { RouterOutlet } from '@angular/router';
-
+import { Observable } from 'rxjs';
+export interface ProductModel {
+  readonly fitness: string
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FitnessPlaner';
-}
+  private client = inject(AngularFirestore)
+ 
+  getProducts(): Observable<ProductModel[]> {
+    return this.client.collection<ProductModel>('products').valueChanges()
+  }
+  
+  readonly products$: Observable<ProductModel[]> = this.getProducts()
+  }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environmet } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom([
-    AngularFirestoreModule, AngularFireModule.initializeApp(environmet.firebaseConfig)
-  ]),],
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom([
+      AngularFirestoreModule,
+      AngularFireModule.initializeApp(environmet.firebaseConfig),
+    ]),
+  ],
 };
