a1b8561f94db07e8c298b68da396800452880760
your_name <patip8999@gmail.com>
2024-12-17 19:26:09 +0100

    app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environmet } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom([
+    AngularFirestoreModule, AngularFireModule.initializeApp(environmet.firebaseConfig)
+  ]),],
 };
