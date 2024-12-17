de1894f34f0f86e2b217598c2c997039ec2c24f0
your_name <patip8999@gmail.com>
2024-12-17 19:22:58 +0100

    firebase

---
+++ src/environments/environments.ts
@@ -0,0 +1,11 @@
+export const environmet ={
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyA27S8WmqVrZFFO9epniU6KwbjmzmEAt44",
+        authDomain: "fitness-7d0de.firebaseapp.com",
+        projectId: "fitness-7d0de",
+        storageBucket: "fitness-7d0de.firebasestorage.app",
+        messagingSenderId: "868055861373",
+        appId: "1:868055861373:web:681f1ad485b72ebc338914"
+      },
+}
