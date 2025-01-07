c3b79c74e6bf15662c162d404c76be02cefd3a21
your_name <patip8999@gmail.com>
2025-01-07 18:19:55 +0100

    d

--- src/app/Models/training-plan.model.ts
+++ src/app/Models/training-plan.model.ts
@@ -1,16 +1,9 @@
 export interface TrainingPlanModel {
-<<<<<<< HEAD
   description: string;
   id: string;            // ID planu
   name: string;          // Nazwa planu
   days: TrainingPlanDay[]; // Lista dni w planie
   uid: string;           // UID użytkownika, który stworzył plan
-=======
-  id: string;            
-  name: string;          
-  days: TrainingPlanDay[]; 
-  uid: string;           
->>>>>>> df169bdb9b84beecfcc25e58a2ec9d811049cb3d
   
 }
 
