144bbf5d8c2c8c427dc4ff2e44746898e17dab99
your_name <patip8999@gmail.com>
2025-02-11 17:55:51 +0100

    signal

--- src/app/Components/monthly-summary/monthly-summary.component.ts
+++ src/app/Components/monthly-summary/monthly-summary.component.ts
@@ -1,87 +1,69 @@
-import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed } from '@angular/core';
 import { TrainingAndMealService } from '../../Services/calendar.service';
 import { DatePipe } from '@angular/common';
 import { TrainingModel } from '../../Models/training.model';
 import { mealModel } from '../../Models/meal.model';
 import { WeightControlComponent } from '../weight-control/weight-control.component';
-import { take } from 'rxjs';
+
 @Component({
   selector: 'app-monthly-summary',
   standalone: true,
   imports: [DatePipe, WeightControlComponent],
   templateUrl: './monthly-summary.component.html',
   styleUrls: ['./monthly-summary.component.css'],
-  
 })
 export class MonthlySummaryComponent {
   meals = signal<mealModel[]>([]);
   trainings = signal<TrainingModel[]>([]);
-  currentMonth: Date = new Date();
-  isDataLoaded = false;
+  currentMonth = signal<Date>(new Date());
 
-  trainingAndMealService: TrainingAndMealService = inject(
-    TrainingAndMealService
-  );
-  cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+  trainingAndMealService: TrainingAndMealService = inject(TrainingAndMealService);
 
   constructor() {
     this.loadMonthlyDetails();
   }
 
   changeMonth(offset: number): void {
-    const newMonth = new Date(this.currentMonth);
-    newMonth.setMonth(this.currentMonth.getMonth() + offset);
-    
-    // Sprawdzanie, czy nowy miesiąc jest różny od obecnego
-    if (newMonth.getTime() !== this.currentMonth.getTime()) {
-      this.currentMonth = newMonth;
-      this.loadMonthlyDetails();
-      this.cdr.markForCheck();
-    }
+    const newMonth = new Date(this.currentMonth());
+    newMonth.setMonth(newMonth.getMonth() + offset);
+    this.currentMonth.set(newMonth);
+    this.loadMonthlyDetails();
   }
+
   loadMonthlyDetails(): void {
-    console.log('Loading monthly details for month:', this.currentMonth);
-  
     const startOfMonth = new Date(
-      this.currentMonth.getFullYear(),
-      this.currentMonth.getMonth(),
+      this.currentMonth().getFullYear(),
+      this.currentMonth().getMonth(),
       1
     );
     const endOfMonth = new Date(
-      this.currentMonth.getFullYear(),
-      this.currentMonth.getMonth() + 1,
+      this.currentMonth().getFullYear(),
+      this.currentMonth().getMonth() + 1,
       0
     );
-  
+
     this.trainingAndMealService
       .getMealsByDateRange(startOfMonth, endOfMonth)
-      .pipe(take(1))  // Zakończenie subskrypcji po pierwszym elemencie
       .subscribe((meals) => {
-        console.log('Meals loaded:', meals);
         this.meals.set(meals || []);
       });
-  
+
     this.trainingAndMealService
       .getTrainingsByDateRange(startOfMonth, endOfMonth)
-      .pipe(take(1))  // Zakończenie subskrypcji po pierwszym elemencie
       .subscribe((trainings) => {
-        console.log('Trainings loaded:', trainings);
         this.trainings.set(trainings || []);
       });
   }
-  
-  totalCalories(): number {
-    const meals = this.meals();
-    return meals.reduce(
+
+  totalCalories = computed(() => {
+    return this.meals().reduce(
       (sum, meal) => sum + (meal.calories ? Number(meal.calories) : 0),
       0
     );
-  }
+  });
 
-  totalBurnedCalories(): number {
-    const trainings = this.trainings();
-
-    const completedTrainings = trainings.filter(
+  totalBurnedCalories = computed(() => {
+    const completedTrainings = this.trainings().filter(
       (training) => training.isDone === true
     );
 
@@ -90,19 +72,16 @@ export class MonthlySummaryComponent {
         sum + (training.burnedKcal ? Number(training.burnedKcal) : 0),
       0
     );
-  }
-  totalTrainingTime(): number {
-    const trainings = this.trainings();
-    const completedTrainings = trainings.filter(
+  });
+
+  totalTrainingTime = computed(() => {
+    const completedTrainings = this.trainings().filter(
       (training) => training.isDone === true
     );
 
-    console.log('Completed Trainings:', completedTrainings);
-
     return completedTrainings.reduce((sum, training) => {
       const time = training.time ? Number(training.time) : 0;
-      console.log('Training Time:', time);
       return sum + (isNaN(time) ? 0 : time);
     }, 0);
-  }
+  });
 }
